e14ef068b363c3eaba804a2bb99756d506d0033d
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-02-02 11:18:59 -0600

    initial deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,61 +2,46 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-    lib: {
-      entry: {
-        buttons: "lib/components/buttons/index.ts",
-        composite: "lib/components/composite/index.ts",
-        core: "lib/components/core/index.ts",
-        display: "lib/components/display/index.js",
-        layout: "lib/components/layout/index.js",
-        navigation: "lib/components/navigation/index.ts",
-        theme: "lib/components/theme/index.ts",
-        main: "index.html",
-      },
-    },
-
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+export default defineConfig(({ mode }) => {
+  if (mode === "lib") {
+    console.log("Building library");
+    return {
+      plugins: [react()],
+      publicDir: false,
+      build: {
+        lib: {
+          entry: {
+            buttons: "lib/components/buttons/index.ts",
+            composite: "lib/components/composite/index.ts",
+            core: "lib/components/core/index.ts",
+            display: "lib/components/display/index.js",
+            layout: "lib/components/layout/index.js",
+            navigation: "lib/components/navigation/index.ts",
+            theme: "lib/components/theme/index.ts",
+            main: "index.html",
+          },
+        },
+        rollupOptions: {
+          external: ["react", "react-dom"],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+            },
+          },
         },
       },
-    },
-  },
+    };
+  } else {
+    console.log("Building preview app");
+    const base =
+      mode === "production" ? "https://usace.github.io/groundwork/" : "";
+    return {
+      plugins: [react()],
+      base: base,
+      build: {
+        outDir: "docs",
+      },
+    };
+  }
 });
-
-// export default defineConfig(({ command }) => {
-//   const config = {
-//     plugins: [react()],
-//   };
-
-//   if(command === 'build'){
-//     const homepage = "https://usace.github.io/groundwork";
-//     return {
-//       ...config,
-//       ...{
-//       lib: {
-//         entry: "lib/index.ts",
-//         name: "Groundwork",
-//         fileName: (format) => `groundwork.${format}.js`,
-//       },
-//       rollupOptions: {
-//         external: ["react", "react-dom"],
-//         output: {
-//           globals: {
-//             react: "React",
-//             "react-dom": "ReactDOM",
-//           },
-//         },
-//       },
-//     }
-//     }
-//   }
-// });
