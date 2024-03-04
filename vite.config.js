b48ea3ca44a171da4015aae0b5ac97afa663389a
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-03-04 13:44:00 -0600

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ mode }) => {
+  if (mode === "lib") {
+    console.log("Building library");
+    return {
+      plugins: [react()],
+      publicDir: false,
+      build: {
+        lib: {
+          name: "Groundwork",
+          fileName: (format) => `groundwork.${format}.js`,
+          entry: "lib/index.jsx",
+        },
+        rollupOptions: {
+          external: ["react", "react-dom"],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+            },
+          },
+        },
+      },
+    };
+  } else {
+    console.log("Building preview app", mode);
+    const base =
+      mode === "production"
+        ? "https://usace.github.io/groundwork/"
+        : "http://localhost:5173/";
+    return {
+      plugins: [react()],
+      base: base,
+      build: {
+        outDir: "docs",
+      },
+    };
+  }
+});
