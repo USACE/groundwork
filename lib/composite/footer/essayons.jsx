3aa3a7123c7978c37c525c2769ddfffdeeb9b3cc
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-03-08 20:55:22 -0600

    fix essayons crest

--- lib/composite/footer/essayons.jsx
+++ lib/composite/footer/essayons.jsx
@@ -3,7 +3,10 @@ const base = import.meta.env.BASE_URL;
 function Essayons() {
   return (
     <div
-      className={`absolute bottom-[56px] left-[50%] h-[125px] w-[112px] ml-[-56px] bg-[url(${base}essayonscrest.png)]`}
+      className={`absolute bottom-[56px] left-[50%] h-[125px] w-[112px] ml-[-56px]`}
+      style={{
+        backgroundImage: `url(${base}essayonscrest.png)`,
+      }}
     />
   );
 }
