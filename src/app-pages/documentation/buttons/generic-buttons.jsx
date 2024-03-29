716b44bfbf95098481ac16e533e655835122f2bc
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-03-29 09:06:30 -0500

    update button docs

--- src/app-pages/documentation/buttons/generic-buttons.jsx
+++ src/app-pages/documentation/buttons/generic-buttons.jsx
@@ -24,20 +24,14 @@ const componentProps = [
   {
     name: "color",
     type: "string",
-    default: "'dark/zinc'",
-    desc: "The solid color variant to use.",
+    default: "'primary'",
+    desc: "The color variant to use.",
   },
   {
-    name: "outline",
-    type: "boolean",
-    default: "false",
-    desc: "Render a button with an outline but no background.",
-  },
-  {
-    name: "plain",
-    type: "boolean",
-    default: "false",
-    desc: "Render a plain button with no background or outline.",
+    name: "style",
+    type: "string",
+    default: "'filled'",
+    desc: "The style variant to use. Options: 'filled', 'outline', 'plain'.",
   },
   {
     name: "disabled",
@@ -51,6 +45,54 @@ const componentProps = [
     default: "undefined",
     desc: "Handler that will fire on the click event, all other basic React event handlers are available as well.",
   },
+  {
+    name: "size",
+    type: "string",
+    default: "'md'",
+    desc: "The size variant to use. Options: 'xs', 'sm', 'md', 'lg', 'xl'.",
+  },
+  {
+    name: "radius",
+    type: "string",
+    default: "'md'",
+    desc: "The radius variant to use. Options: 'none', 'sm', 'md', 'lg', 'xl'.",
+  },
+  {
+    name: "href",
+    type: "string",
+    default: "undefined",
+    desc: "Add a url to render the button as a link.",
+  },
+];
+
+const colors = [
+  "light",
+  "dark",
+  "primary",
+  "secondary",
+  "success",
+  "warning",
+  "danger",
+  "info",
+  "white",
+  "zinc",
+  "indigo",
+  "cyan",
+  "red",
+  "orange",
+  "amber",
+  "yellow",
+  "lime",
+  "green",
+  "emerald",
+  "teal",
+  "sky",
+  "blue",
+  "violet",
+  "purple",
+  "fuchsia",
+  "pink",
+  "rose",
 ];
 
 function GenericButtonsDocs() {
@@ -71,8 +113,8 @@ function GenericButtonsDocs() {
         <div className="rounded-md border border-dashed px-6 py-3 mb-3">
           <div className="flex flex-row flex-wrap gap-3">
             <Button>Default</Button>
-            <Button outline>Outlined</Button>
-            <Button plain>Plain</Button>
+            <Button style="outline">Outlined</Button>
+            <Button style="plain">Plain</Button>
             <Button disabled>Disabled</Button>
             <Button href="/docs/buttons">As a Link</Button>
           </div>
@@ -85,8 +127,8 @@ function Component() {
   return (
     <div className="flex flex-row flex-wrap gap-3">
       <Button>Default</Button>
-      <Button outline>Outlined</Button>
-      <Button plain>Plain</Button>
+      <Button style="outline">Outlined</Button>
+      <Button style="plain">Plain</Button>
       <Button disabled>Disabled</Button>
       <Button href="/docs/buttons">As a Link</Button>
     </div>
@@ -97,34 +139,10 @@ export default Component;
 `}
         />
         {/* Example usage - remove if not needed */}
-        <H3 className="pt-6 pb-3">Built-in Color Options</H3>
+        <H3 className="pt-6 pb-3">Built-in Color Options - Filled</H3>
         <div className="rounded-md border border-dashed px-6 py-3 mb-3">
           <div className="flex flex-row flex-wrap gap-3">
-            {[
-              "dark/zinc",
-              "light",
-              "dark/white",
-              "dark",
-              "white",
-              "zinc",
-              "indigo",
-              "cyan",
-              "red",
-              "orange",
-              "amber",
-              "yellow",
-              "lime",
-              "green",
-              "emerald",
-              "teal",
-              "sky",
-              "blue",
-              "violet",
-              "purple",
-              "fuchsia",
-              "pink",
-              "rose",
-            ].map((clr) => {
+            {colors.map((clr) => {
               return (
                 <Button key={clr} color={clr}>
                   {clr}
@@ -136,6 +154,8 @@ export default Component;
         {/* Example code */}
         <CodeExample
           code={`import { Button } from "@usace/groundwork";
+
+const colors = [...];
 
 function Component() {
   return (
@@ -154,6 +174,147 @@ function Component() {
 export default Component;
 `}
         />
+
+        <H3 className="pt-6 pb-3">Built-in Color Options - Outline</H3>
+        <div className="rounded-md border border-dashed px-6 py-3 mb-3">
+          <div className="flex flex-row flex-wrap gap-3">
+            {colors.map((clr) => {
+              return (
+                <Button key={clr} style="outline" color={clr}>
+                  {clr}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+        {/* Example code */}
+        <CodeExample
+          code={`import { Button } from "@usace/groundwork";
+
+const colors = [...];
+
+function Component() {
+  return (
+    <div className="flex flex-row flex-wrap gap-3">
+      {colors.map((clr) => {
+        return (
+          <Button key={clr} style="outline" color={clr}>
+            {clr}
+          </Button>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Component;
+`}
+        />
+
+        <H3 className="pt-6 pb-3">Built-in Color Options - Plain</H3>
+        <div className="rounded-md border border-dashed px-6 py-3 mb-3">
+          <div className="flex flex-row flex-wrap gap-3">
+            {colors.map((clr) => {
+              return (
+                <Button key={clr} style="plain" color={clr}>
+                  {clr}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+        {/* Example code */}
+        <CodeExample
+          code={`import { Button } from "@usace/groundwork";
+
+const colors = [...];
+
+function Component() {
+  return (
+    <div className="flex flex-row flex-wrap gap-3">
+      {colors.map((clr) => {
+        return (
+          <Button key={clr} style="plain" color={clr}>
+            {clr}
+          </Button>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Component;
+`}
+        />
+
+        <H3 className="pt-6 pb-3">Built-in Size Options</H3>
+        <div className="rounded-md border border-dashed px-6 py-3 mb-3">
+          <div className="">
+            {["xs", "sm", "md", "lg", "xl"].map((size) => {
+              return (
+                <Button key={size} size={size} className="mr-3">
+                  {size}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+        {/* Example code */}
+        <CodeExample
+          code={`import { Button } from "@usace/groundwork";
+
+function Component() {
+  return (
+    <div className="flex flex-row flex-wrap gap-3">
+      {["xs", "sm", "md", "lg", "xl"].map((size) => {
+        return (
+          <Button key={size} size={size} className="mr-3">
+            {size}
+          </Button>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Component;
+`}
+        />
+
+        <H3 className="pt-6 pb-3">Built-in Radius Options</H3>
+        <div className="rounded-md border border-dashed px-6 py-3 mb-3">
+          <div className="">
+            {["none", "sm", "md", "lg", "xl"].map((size) => {
+              return (
+                <Button key={size} radius={size} className="mr-3">
+                  {size}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+        {/* Example code */}
+        <CodeExample
+          code={`import { Button } from "@usace/groundwork";
+
+function Component() {
+  return (
+    <div className="flex flex-row flex-wrap gap-3">
+      {["none", "sm", "md", "lg", "xl"].map((size) => {
+        return (
+          <Button key={size} radius={size} className="mr-3">
+            {size}
+          </Button>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Component;
+`}
+        />
+
         {/* Component props documentation */}
         <div className="font-bold text-lg pt-6">
           Component API - <Code className="p-2">{`<SiteWrapper />`}</Code>
