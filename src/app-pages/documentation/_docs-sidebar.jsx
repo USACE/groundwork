c0423df1cd2bb317b94afebae169217ab79ab703
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-04-03 11:48:43 -0500

    abstract out the demo app links since we're using them in multiple places

--- src/app-pages/documentation/_docs-sidebar.jsx
+++ src/app-pages/documentation/_docs-sidebar.jsx
@@ -1,163 +1,7 @@
 import { useConnect } from "redux-bundler-hook";
 import { UsaceBox } from "../../../lib";
 import { VscChevronRight } from "react-icons/vsc";
-
-const sidebarLinks = [
-  {
-    title: "Getting Started",
-    href: "/docs",
-  },
-  {
-    title: "Application Shell",
-    href: "/docs/app-shell",
-    children: [
-      {
-        title: "Site Wrapper",
-        href: "/docs/app-shell/site-wrapper",
-      },
-      {
-        title: "Search",
-        href: "/docs/app-shell/search",
-      },
-    ],
-  },
-  {
-    title: "Layout",
-    href: "/docs/layout",
-    children: [
-      {
-        title: "Container",
-        href: "/docs/layout/container",
-      },
-      {
-        title: "UsaceBox",
-        href: "/docs/layout/usace-box",
-      },
-    ],
-  },
-  {
-    title: "Navigation",
-    href: "/docs/navigation",
-    children: [
-      {
-        title: "Breadcrumbs",
-        href: "/docs/navigation/breadcrumbs",
-      },
-      {
-        title: "Tabs",
-        href: "/docs/navigation/tabs",
-      },
-    ],
-  },
-  {
-    title: "Buttons",
-    href: "/docs/buttons",
-    children: [
-      {
-        title: "Generic Buttons",
-        href: "/docs/buttons/generic-buttons",
-      },
-      {
-        title: "Ok/Cancel",
-        href: "/docs/buttons/ok-cancel",
-      },
-      {
-        title: "Delete/Confirm",
-        href: "/docs/buttons/delete-confirm",
-      },
-      {
-        title: "Login Button",
-        href: "/docs/buttons/login-button",
-      },
-    ],
-  },
-  {
-    title: "Display",
-    href: "/docs/display",
-    children: [
-      {
-        title: "Badge",
-        href: "/docs/display/badge",
-      },
-      {
-        title: "Headings",
-        href: "/docs/display/headings",
-      },
-      {
-        title: "Hero",
-        href: "/docs/display/hero",
-      },
-      {
-        title: "Text",
-        href: "/docs/display/text",
-      },
-      {
-        title: "Table",
-        href: "/docs/display/table",
-      },
-      {
-        title: "Accordion",
-        href: "/docs/display/accordion",
-      },
-      {
-        title: "Card",
-        href: "/docs/display/card",
-      },
-    ],
-  },
-  {
-    title: "Forms",
-    href: "/docs/forms",
-    children: [
-      {
-        title: "Fieldset",
-        href: "/docs/forms/fieldset",
-      },
-      {
-        title: "Text Inputs",
-        href: "/docs/forms/text-inputs",
-      },
-      {
-        title: "Text Area",
-        href: "/docs/forms/textarea",
-      },
-      {
-        title: "Numeric Inputs",
-        href: "/docs/forms/numeric-inputs",
-      },
-      {
-        title: "Color Input",
-        href: "/docs/forms/color-input",
-      },
-      {
-        title: "Date/Time Inputs",
-        href: "/docs/forms/date-time-inputs",
-      },
-      {
-        title: "File Input",
-        href: "/docs/forms/file-input",
-      },
-    ],
-  },
-  {
-    title: "Water Management",
-    href: "/docs/water-management",
-  },
-  {
-    title: "Types",
-    href: "/docs/types",
-    children: [
-      {
-        title: "Link",
-        href: "/docs/types/link",
-      },
-      {
-        title: "Tab",
-        href: "/docs/types/tab",
-      },
-    ],
-  },
-];
+import sidebarLinks from "../../nav-links";
 
 function DocsSidebar() {
   const { hash } = useConnect("selectHash");
@@ -172,7 +16,7 @@ function DocsSidebar() {
                   hash === link.href ? "bg-gray-50 rounded" : ""
                 }`}
               >
-                {link.title}
+                {link.text}
                 <VscChevronRight
                   size={18}
                   aria-hidden="true"
@@ -190,7 +34,7 @@ function DocsSidebar() {
                           hash === child.href ? "bg-gray-50 rounded" : ""
                         }`}
                       >
-                        {child.title}
+                        {child.text}
                         <VscChevronRight
                           size={18}
                           aria-hidden="true"
