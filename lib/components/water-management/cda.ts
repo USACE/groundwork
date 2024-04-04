491d587467a285f08631da22e6a472e29a1f2d7b
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-04-04 10:34:13 -0400

    Move shared CDA items to cda.ts
    Also fix language of buildRequest to no longer indicate that
    the "office" param is filled automatically.

---
+++ lib/components/water-management/cda.ts
@@ -0,0 +1,13 @@
+const CDA_URL: string = "https://localhost:7000/lrl-data";
+
+/**
+ * Create a CDA request URL string by appending provided endpoint and parameters
+ * to the base CDA_URL.
+ * @param endpoint The "base" of the API call (before parameters).
+ * @param paramStr Request parameters in the form of "parameter=value&parameter=value".
+ * @returns A full CDA request URL string.
+ */
+export const buildRequest = (endpoint: string, paramStr: string) => {
+  let cdaRequest = CDA_URL + endpoint + "?" + paramStr;
+  return cdaRequest;
+};
