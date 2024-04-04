a3bec09cde86fc71d56ca49376c15806df6fda15
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-04-04 11:26:52 -0500

    move className to actually monkeypatch classes right

--- lib/components/table.jsx
+++ lib/components/table.jsx
@@ -26,8 +26,8 @@ export function Table({
         <div
           {...props}
           className={clsx(
-            className,
-            "-mx-[--gutter] overflow-x-auto whitespace-nowrap"
+            "-mx-[--gutter] overflow-x-auto whitespace-nowrap",
+            className
           )}
         >
           <div
@@ -47,7 +47,7 @@ export function Table({
 export function TableHead({ className, ...props }) {
   return (
     <thead
-      className={clsx(className, "text-zinc-500 dark:text-zinc-400")}
+      className={clsx("text-zinc-500 dark:text-zinc-400", className)}
       {...props}
     />
   );
@@ -78,14 +78,14 @@ export function TableRow({
       <tr
         {...props}
         className={clsx(
-          className,
           href &&
             "has-[[data-row-link][data-focus]]:outline has-[[data-row-link][data-focus]]:outline-2 has-[[data-row-link][data-focus]]:-outline-offset-2 has-[[data-row-link][data-focus]]:outline-blue-500 dark:focus-within:bg-white/[2.5%]",
           striped && "even:bg-zinc-950/[2.5%] dark:even:bg-white/[2.5%]",
           href && striped && "hover:bg-zinc-950/5 dark:hover:bg-white/5",
           href &&
             !striped &&
-            "hover:bg-zinc-950/[2.5%] dark:hover:bg-white/[2.5%]"
+            "hover:bg-zinc-950/[2.5%] dark:hover:bg-white/[2.5%]",
+          className
         )}
       >
         {children}
@@ -101,11 +101,11 @@ export function TableHeader({ className, ...props }) {
     <th
       {...props}
       className={clsx(
-        className,
         "border-b border-b-zinc-950/10 px-4 py-2 font-medium first:pl-[var(--gutter,theme(spacing.2))] last:pr-[var(--gutter,theme(spacing.2))] dark:border-b-white/10",
         grid &&
           "border-l border-l-zinc-950/5 first:border-l-0 dark:border-l-white/5",
-        !bleed && "sm:first:pl-2 sm:last:pr-2"
+        !bleed && "sm:first:pl-2 sm:last:pr-2",
+        className
       )}
     />
   );
@@ -121,13 +121,13 @@ export function TableCell({ className, children, ...props }) {
       ref={href ? setCellRef : undefined}
       {...props}
       className={clsx(
-        className,
         "relative px-4 first:pl-[var(--gutter,theme(spacing.2))] last:pr-[var(--gutter,theme(spacing.2))]",
         !striped && "border-b border-zinc-950/5 dark:border-white/5",
         grid &&
           "border-l border-l-zinc-950/5 first:border-l-0 dark:border-l-white/5",
         dense ? "py-2.5" : "py-4",
-        !bleed && "sm:first:pl-2 sm:last:pr-2"
+        !bleed && "sm:first:pl-2 sm:last:pr-2",
+        className
       )}
     >
       {href && (
