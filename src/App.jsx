d7efd5df49a569d3be92ba119de928795520e648
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-04-04 12:46:36 -0500

    add link to the gh repo in the navbar for the demo site (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import {
   SearchDotGov,
   LoginButton,
   ProfileDropdown,
+  Button,
 } from "../lib";
 import { getNavHelper } from "internal-nav-helper";
 import { useConnect } from "redux-bundler-hook";
 import links from "./nav-links";
+import { FaGithub } from "react-icons/fa";
 
 const version = import.meta.env.PKG_VERSION;
 
@@ -38,6 +40,15 @@ function App() {
               affiliate="groundwork"
               accessKey="JdBfW2_sGkdcLr4BTzCoOQIRy3oRP7kmzJ2DwIs-SCM="
             />
+            <Button
+              style="plain"
+              color="white"
+              size="lg"
+              href="https://github.com/usace/groundwork"
+              title="Check out the source code on GitHub"
+            >
+              <FaGithub />
+            </Button>
           </>
         }
       >
