7ed5c53bafe6ea13ccaff19c60f3244a7750d5bb
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-04-05 08:50:44 -0400

    Add a ParamsTable for hook params definitions

---
+++ src/app-components/params-table.jsx
@@ -0,0 +1,48 @@
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableHeader,
+  TableCell,
+  Text,
+  Code,
+} from "../../lib";
+
+function ParamsTable({ paramsList }) {
+  if (!paramsList || !paramsList.length)
+    return (
+      <Text className="pt-3">This hook does not accept any parameters.</Text>
+    );
+  return (
+    <Table striped dense>
+      <TableHead>
+        <TableRow>
+          <TableHeader>Parameter</TableHeader>
+          <TableHeader>Type</TableHeader>
+          <TableHeader>Required?</TableHeader>
+          <TableHeader>Description</TableHeader>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {paramsList.map((param) => (
+          <TableRow key={param.name}>
+            <TableCell>
+              <Code>{param.name}</Code>
+            </TableCell>
+            <TableCell>
+              <Code>{param.type}</Code>
+            </TableCell>
+            <TableCell>
+              <Code>{param.required ? "required" : "optional"}</Code>
+            </TableCell>
+            <TableCell className="text-wrap">{param.desc}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+export default ParamsTable;
+export { ParamsTable as PropsTable };
