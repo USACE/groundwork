707291a0884978efac64a9d9514c23613c536785
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2024-04-18 11:45:16 -0400

    forgot two files - back to double quotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ReduxBundlerProvider } from 'redux-bundler-hook';
-import getStore from './app-bundles';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ReduxBundlerProvider } from "redux-bundler-hook";
+import getStore from "./app-bundles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
+import "./index.css";
 
 const store = getStore();
 
-if (import.meta.env.MODE === 'development') window.store = store;
+if (import.meta.env.MODE === "development") window.store = store;
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ReduxBundlerProvider store={store}>
       <QueryClientProvider client={queryClient}>
