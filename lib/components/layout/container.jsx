51ef0ee804153c7075908ea1473a62f9319db16c
Charles Graham <charles.r.graham@usace.army.mil>
2024-06-20 22:59:12 -0500

    Move sidebar component to composite, update docs page with change. Add sidebar component #78

--- lib/components/layout/container.jsx
+++ lib/components/layout/container.jsx
@@ -1,12 +1,41 @@
 import clsx from "clsx";
+import { Breadcrumbs, BreadcrumbItem, Sidebar } from "../../../lib";
 
-function Container({ className, children, fluid }) {
+function Container({ className, children, fluid, breadcrumbs, sidebarLinks }) {
   const containerClass = clsx(
     "gw-w-full gw-mx-auto gw-px-4 gw-box-border",
     fluid ? "gw-max-w-screen" : "gw-max-w-screen-2xl",
     className
   );
-  return <div className={containerClass}>{children}</div>;
+
+  const renderBreadcrumbs = () =>
+    breadcrumbs && (
+      <Breadcrumbs>
+        {breadcrumbs.map((breadcrumb) => (
+          <BreadcrumbItem
+            key={breadcrumb.text}
+            href={breadcrumb.href}
+            text={breadcrumb.text}
+          />
+        ))}
+      </Breadcrumbs>
+    );
+
+  return (
+    <div className={containerClass}>
+      {renderBreadcrumbs()}
+      {sidebarLinks ? (
+        <div className="gw-grid gw-grid-cols-12 gw-gap-6">
+          <div className="gw-hidden md:gw-block md:gw-col-span-2">
+            <Sidebar sidebarLinks={sidebarLinks} />
+          </div>
+          <div className="gw-col-span-12 md:gw-col-span-10">{children}</div>
+        </div>
+      ) : (
+        children
+      )}
+    </div>
+  );
 }
 
 export default Container;
--- src/app-pages/documentation/_docs-page.jsx
+++ src/app-pages/documentation/_docs-page.jsx
@@ -5,24 +5,8 @@ import { sidebarLinks } from "../../nav-links";
 function DocsPage({ breadcrumbs = [], children }) {
   return (
     <div>
-      <Container fluid>
-        <Breadcrumbs>
-          {breadcrumbs.map((breadcrumb) => {
-            return (
-              <BreadcrumbItem
-                key={breadcrumb.text}
-                href={breadcrumb.href}
-                text={breadcrumb.text}
-              />
-            );
-          })}
-        </Breadcrumbs>
-        <div className="gw-grid gw-grid-cols-12 gw-gap-6">
-          <div className="gw-hidden md:gw-block md:gw-col-span-2">
-            <Sidebar sidebarLinks={sidebarLinks} />
-          </div>
-          <div className="gw-col-span-12 md:gw-col-span-10">{children}</div>
-        </div>
+      <Container fluid sidebarLinks={sidebarLinks} breadcrumbs={breadcrumbs}>
+        {children}
       </Container>
     </div>
   );
