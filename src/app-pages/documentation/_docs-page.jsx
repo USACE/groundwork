6ff6c528162d0d7b5f370268328d69a1678dcdb7
Charles Graham <charles.r.graham@usace.army.mil>
2024-06-28 16:40:34 -0500

    Bubble the Sidebar / Crumb logic up and outside of Container

--- src/app-pages/documentation/_docs-page.jsx
+++ src/app-pages/documentation/_docs-page.jsx
@@ -1,13 +1,25 @@
-import { Container } from "../../../lib";
+import { Container, Sidebar, Breadcrumbs, BreadcrumbItem} from "../../../lib"; 
 import  sidebarLinks  from "../../nav-links";
 
 function DocsPage({ breadcrumbs = [], children }) {
   return (
-    <div>
-      <Container fluid sidebarLinks={sidebarLinks} breadcrumbs={breadcrumbs}>
-        {children}
+    <Container fluid>
+        <Breadcrumbs>
+        {breadcrumbs.map((breadcrumb) => (
+          <BreadcrumbItem
+            key={breadcrumb.text}
+            href={breadcrumb.href}
+            text={breadcrumb.text}
+          />
+        ))}
+      </Breadcrumbs>
+        <div className="gw-grid gw-grid-cols-12 gw-gap-6">
+          <div className="gw-hidden md:gw-block md:gw-col-span-2">
+            <Sidebar sidebarLinks={sidebarLinks} />
+          </div>
+          <div className="gw-col-span-12 md:gw-col-span-10">{children}</div>
+        </div>
       </Container>
-    </div>
   );
 }
 
