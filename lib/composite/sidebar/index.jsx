543db65b457613961e2ba84525da1497f82b1266
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2024-09-09 00:04:23 -0500

    Bring everything together in the previous commits to dynamically change the sidebar to turn into a dropdown when below the `md` breakpoint used for hiding sidebar.
    Do this in such a way that end users do not have to update their source. (i.e. use javascript to target parent elements)

--- lib/composite/sidebar/index.jsx
+++ lib/composite/sidebar/index.jsx
@@ -1,18 +1,72 @@
 import { UsaceBox, PopoutMenu } from "../../../lib";
 import { VscChevronRight } from "react-icons/vsc";
+import { useIsMobile } from "../../hooks/useIsMobile";
+import Dropdown from "../../components/form/dropdown";
+import { useEffect, useRef } from "react";
+import { useConnect } from "redux-bundler-hook";
 
+const BlackDot = ({ title = "Current Page" }) => (
+  <span
+    title={title}
+    className="gw-inline-block gw-w-2 gw-h-2 gw-rounded-full gw-bg-black gw-mr-1 gw-ms-auto"
+  />
+);
 
-const BlackDot = ({title = "Current Page"}) => (
-  <span title={title} className="gw-inline-block gw-w-2 gw-h-2 gw-rounded-full gw-bg-black gw-mr-1 gw-ms-auto" />
-)
-
-function Sidebar({ title = "Contents", selectedPath, sidebarLinks, enablePopout, popoutDirection}) {
+// TODO: Add handler for when user presses escape to reset state of graphical arrow < and >
+function Sidebar({
+  title = "Contents",
+  selectedPath,
+  sidebarLinks,
+  enablePopout,
+  popoutDirection,
+}) {
+  const isMobile = useIsMobile();
+  const { doUpdateHash } = useConnect("doUpdateHash");
+  const sidebarRef = useRef(null);
   if (popoutDirection && !enablePopout) {
-    throw new Error("popoutDirection can only be used when enablePopout is true");
+    throw new Error(
+      "popoutDirection can only be used when enablePopout is true"
+    );
   }
 
+  useEffect(() => {
+    if (sidebarRef.current) {
+      const parentElement = sidebarRef.current.parentElement;
+      if (
+        parentElement &&
+        parentElement?.parentElement.classList.contains("gw-grid")
+      ) {
+        parentElement?.parentElement.classList.remove("gw-grid");
+        parentElement.classList.remove("gw-hidden");
+      } else parentElement?.parentElement.classList.add("gw-grid");
+    }
+  }, [isMobile]);
+
+  if (isMobile) {
+    return (
+      <>
+        <UsaceBox propRef={sidebarRef} title={title} className={"gw-text-sm"}>
+          <div className="gw-w-full gw-m-auto gw-flex gw-justify-between gw-items-center gw-gap-2">
+            <Dropdown
+              className={"gw-w-5/6 gw-m-auto"}
+              value={selectedPath}
+              onChange={(e) => {
+                doUpdateHash(e.target.value);
+              }}
+              options={sidebarLinks.map((link) => (
+                <option key={link.href} value={link.href}>
+                  {console.log(link.href)}
+                  {link.text}
+                </option>
+              ))}
+            />
+          </div>
+        </UsaceBox>
+      </>
+    );
+  }
   return (
-    <UsaceBox title={title}>
+    <UsaceBox propRef={sidebarRef} title={title}>
       {sidebarLinks.map((link) => {
         const isSelected = selectedPath === link.href;
         return (
@@ -30,7 +84,7 @@ function Sidebar({ title = "Contents", selectedPath, sidebarLinks, enablePopout,
                         }`}
                       >
                         {/* Place a black dot at the end of the link if it is selected */}
-                        {link.text} { isSelected && <BlackDot />}
+                        {link.text} {isSelected && <BlackDot />}
                       </a>
                     }
                     {link.children.map((child) => {
@@ -60,7 +114,6 @@ function Sidebar({ title = "Contents", selectedPath, sidebarLinks, enablePopout,
                     }`}
                   >
                     {link.text}
-                 
                   </div>
                 </a>
               )
