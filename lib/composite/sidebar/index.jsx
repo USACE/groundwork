1204d113b8ac8195cc4b739a723495c28ec2ecff
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2024-09-09 02:10:43 -0500

    Add nesting to popout menus and sidebar without popout.

--- lib/composite/sidebar/index.jsx
+++ lib/composite/sidebar/index.jsx
@@ -13,7 +13,99 @@ const BlackDot = ({ title = "Current Page" }) => (
   />
 );
 
-// TODO: Add handler for when user presses escape to reset state of graphical arrow < and >
+// Recursive function to render nested PopoutMenus
+function renderPopoutMenu(
+  link,
+  selectedPath,
+  enablePopout,
+  popoutDirection,
+  level = 0
+) {
+  const isSelected = selectedPath === link.href;
+  link.level = level; // Track the level of each link
+
+  if (link.children && link.children.length > 0) {
+    return (
+      <div className="gw-py-1 gw-border-b-[1px] gw-border-b-gray-500 hover:gw-bg-gray-100">
+        <PopoutMenu title={link.text} direction={popoutDirection}>
+          {
+            <a
+              key={link.id}
+              href={link.href}
+              className={`gw-flex gw-items-center gw-gap-1 gw-p-2 gw-border-b-[1px] gw-border-b-gray-200 gw-bg-gray-100 gw-font-bold ${
+                isSelected ? "gw-bg-gray-100 gw-rounded" : ""
+              }`}
+            >
+              {link.text} {isSelected && <BlackDot />}
+            </a>
+          }
+          {link.children.map((child) =>
+            renderPopoutMenu(
+              child,
+              selectedPath,
+              enablePopout,
+              popoutDirection,
+              level + 1
+            )
+          )}
+        </PopoutMenu>
+      </div>
+    );
+  }
+
+  return (
+    <a href={link.href} key={link.id}>
+      <div
+        className={`gw-pl-1 ${
+          link?.children || link?.level === 0 ? "gw-text-lg gw-font-bold" : ""
+        } gw-py-1 gw-flex gw-justify-between gw-items-center gw-cursor-pointer hover:gw-bg-gray-100 ${
+          isSelected ? "gw-bg-gray-50 gw-rounded" : ""
+        }`}
+      >
+        {link.text}
+      </div>
+    </a>
+  );
+}
+
+// Recursive function to render nested links without popout menus
+function renderRegularLinks(link, selectedPath, level = 0) {
+  const isSelected = selectedPath === link.href;
+  const indentation = { paddingLeft: `${level * 20}px` };
+
+  return (
+    <div key={link.id}>
+      <a href={link.href}>
+        <div
+          className={`gw-text-lg ${
+            level === 0 ? "gw-font-bold" : ""
+          } gw-pl-1 gw-py-1 gw-flex gw-justify-between gw-items-center gw-cursor-pointer hover:gw-bg-gray-100 ${
+            isSelected ? "gw-bg-gray-100 gw-rounded" : ""
+          }`}
+          style={indentation}
+        >
+          {link.text}
+          {isSelected && <BlackDot />}
+          {link.children && (
+            <VscChevronRight
+              size={18}
+              aria-hidden="true"
+              color="rgb(156 163 175)"
+            />
+          )}
+        </div>
+      </a>
+      {link.children && (
+        <div>
+          {link.children.map((child) =>
+            renderRegularLinks(child, selectedPath, level + 1)
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
 function Sidebar({
   title = "Contents",
   selectedPath,
@@ -52,120 +144,31 @@ function Sidebar({
       return link;
     });
     return (
-      <>
-        <UsaceBox propRef={sidebarRef} title={title} className={"gw-text-sm"}>
-          <div className="gw-w-full gw-m-auto gw-flex gw-justify-between gw-items-center gw-gap-2">
-            <Dropdown
-              className={"gw-w-5/6 gw-m-auto"}
-              value={selectedPath}
-              onChange={(e) => {
-                doUpdateHash(e.target.value);
-              }}
-              options={combinedLinks.map((link) => (
-                <option key={link.href} value={link.href}>
-                  {link?.path ? link.path + " - " + link.text : link.text}
-                </option>
-              ))}
-            />
-          </div>
-        </UsaceBox>
-      </>
+      <UsaceBox propRef={sidebarRef} title={title} className={"gw-text-sm"}>
+        <div className="gw-w-full gw-m-auto gw-flex gw-justify-between gw-items-center gw-gap-2">
+          <Dropdown
+            className={"gw-w-5/6 gw-m-auto"}
+            value={selectedPath}
+            onChange={(e) => {
+              doUpdateHash(e.target.value);
+            }}
+            options={combinedLinks.map((link) => (
+              <option key={link.href} value={link.href}>
+                {link?.path ? link.path + " - " + link.text : link.text}
+              </option>
+            ))}
+          />
+        </div>
+      </UsaceBox>
     );
   }
+
   return (
     <UsaceBox propRef={sidebarRef} title={title}>
       {sidebarLinks.map((link) => {
-        const isSelected = selectedPath === link.href;
-        return (
-          <div key={link.href}>
-            {enablePopout ? (
-              (link.children?.length && (
-                <div className="gw-py-1 gw-border-b-[1px] gw-border-b-gray-500 hover:gw-bg-gray-100">
-                  <PopoutMenu title={link.text} direction={popoutDirection}>
-                    {
-                      <a
-                        key={link.id}
-                        href={link.href}
-                        className={`gw-flex gw-items-center gw-gap-1 gw-font-bold gw-p-2 gw-border-b-[1px] gw-border-b-gray-200 gw-bg-gray-100 ${
-                          isSelected ? "gw-bg-gray-100 gw-rounded" : ""
-                        }`}
-                      >
-                        {/* Place a black dot at the end of the link if it is selected */}
-                        {link.text} {isSelected && <BlackDot />}
-                      </a>
-                    }
-                    {link.children.map((child) => {
-                      const isChildSelected = selectedPath === child.href;
-                      return (
-                        <a
-                          key={child.id}
-                          href={child.href}
-                          className={`gw-block gw-p-2 gw-border-b-[1px] gw-border-b-gray-200 hover:gw-bg-gray-100 ${
-                            isChildSelected ? "gw-bg-gray-100 gw-rounded" : ""
-                          }`}
-                        >
-                          <span className="gw-flex gw-items-center gw-gap-1">
-                            {child.text}
-                            {isChildSelected && <BlackDot />}
-                          </span>
-                        </a>
-                      );
-                    })}
-                  </PopoutMenu>
-                </div>
-              )) || (
-                <a href={link.href}>
-                  <div
-                    className={`gw-text-lg gw-font-bold gw-pl-1 gw-py-1 gw-flex gw-justify-between gw-items-center gw-cursor-pointer hover:gw-bg-gray-100 ${
-                      isSelected ? "gw-bg-gray-50 gw-rounded" : ""
-                    }`}
-                  >
-                    {link.text}
-                  </div>
-                </a>
-              )
-            ) : (
-              <>
-                <a href={link.href}>
-                  <div
-                    className={`gw-text-lg gw-font-bold gw-pl-1 gw-py-1 gw-flex gw-justify-between gw-items-center gw-cursor-pointer hover:gw-bg-gray-100 ${
-                      isSelected ? "gw-bg-gray-50 gw-rounded" : ""
-                    }`}
-                  >
-                    {link.text}
-                    <VscChevronRight
-                      size={18}
-                      aria-hidden="true"
-                      color="rgb(156 163 175)"
-                    />
-                  </div>
-                </a>
-                <ul>
-                  {link.children &&
-                    link.children.map((child) => {
-                      const isChildSelected = selectedPath === child.href;
-                      return (
-                        <a href={child.href} key={child.href}>
-                          <li
-                            className={`gw-pl-3 gw-py-1 gw-flex gw-justify-between gw-items-center gw-cursor-pointer hover:gw-bg-gray-100 ${
-                              isChildSelected ? "gw-bg-gray-50 gw-rounded" : ""
-                            }`}
-                          >
-                            {child.text}
-                            <VscChevronRight
-                              size={12}
-                              aria-hidden="true"
-                              color="rgb(156 163 175)"
-                            />
-                          </li>
-                        </a>
-                      );
-                    })}
-                </ul>
-              </>
-            )}
-          </div>
-        );
+        return enablePopout
+          ? renderPopoutMenu(link, selectedPath, enablePopout, popoutDirection)
+          : renderRegularLinks(link, selectedPath);
       })}
     </UsaceBox>
   );
