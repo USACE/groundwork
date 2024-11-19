26db09e32633c45ad21dc5f05c07f1a7dd5cdd23
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2024-11-18 23:33:11 -0600

    Attempt to fix Sidebar Issues #120 item 2 - problem with scroll element hiding new popouts for large menu

--- src/example-links.js
+++ src/example-links.js
@@ -26,13 +26,11 @@ export default [
           },
         ],
       },
-    ],
-  },
-  {
-    id: "l6",
-    text: "Link 6",
-    href: "#link6",
-    children: [
+      {
+        id: "l6",
+        text: "Child Link 6",
+        href: "#childlink6",
+      },
       {
         id: "l7",
         text: "Child Link 7",
@@ -42,16 +40,353 @@ export default [
         id: "l8",
         text: "Child Link 8",
         href: "#childlink8",
+      },
+      {
+        id: "l9",
+        text: "Child Link 9",
+        href: "#childlink9",
+      },
+      {
+        id: "l10",
+        text: "Child Link 10",
+        href: "#childlink10",
+      },
+      {
+        id: "l11",
+        text: "Child Link 11",
+        href: "#childlink11",
+      },
+      {
+        id: "l12",
+        text: "Child Link 12",
+        href: "#childlink12",
+      },
+      {
+        id: "l13",
+        text: "Child Link 13",
+        href: "#childlink13",
+      },
+      {
+        id: "l14",
+        text: "Child Link 14",
+        href: "#childlink14",
+      },
+      {
+        id: "l6",
+        text: "Child Link 6",
+        href: "#childlink6",
+      },
+      {
+        id: "l7",
+        text: "Child Link 7",
+        href: "#childlink7",
+      },
+      {
+        id: "l8",
+        text: "Child Link 8",
+        href: "#childlink8",
+      },
+      {
+        id: "l9",
+        text: "Child Link 9",
+        href: "#childlink9",
+      },
+      {
+        id: "l10",
+        text: "Child Link 10",
+        href: "#childlink10",
+      },
+      {
+        id: "l11",
+        text: "Child Link 11",
+        href: "#childlink11",
+      },
+      {
+        id: "l12",
+        text: "Child Link 12",
+        href: "#childlink12",
+      },
+      {
+        id: "l13",
+        text: "Child Link 13",
+        href: "#childlink13",
+      },
+      {
+        id: "l14",
+        text: "Child Link 14",
+        href: "#childlink14",
+      },
+      {
+        id: "l6",
+        text: "Child Link 6",
+        href: "#childlink6",
+      },
+      {
+        id: "l7",
+        text: "Child Link 7",
+        href: "#childlink7",
+      },
+      {
+        id: "l8",
+        text: "Child Link 8",
+        href: "#childlink8",
+      },
+      {
+        id: "l9",
+        text: "Child Link 9",
+        href: "#childlink9",
+      },
+      {
+        id: "l10",
+        text: "Child Link 10",
+        href: "#childlink10",
+      },
+      {
+        id: "l11",
+        text: "Child Link 11",
+        href: "#childlink11",
+      },
+      {
+        id: "l12",
+        text: "Child Link 12",
+        href: "#childlink12",
+      },
+      {
+        id: "l13",
+        text: "Child Link 13",
+        href: "#childlink13",
+      },
+      {
+        id: "l14",
+        text: "Child Link 14",
+        href: "#childlink14",
+      },
+      {
+        id: "l6",
+        text: "Child Link 6",
+        href: "#childlink6",
+      },
+      {
+        id: "l7",
+        text: "Child Link 7",
+        href: "#childlink7",
+      },
+      {
+        id: "l8",
+        text: "Child Link 8",
+        href: "#childlink8",
+      },
+      {
+        id: "l9",
+        text: "Child Link 9",
+        href: "#childlink9",
+      },
+      {
+        id: "l10",
+        text: "Child Link 10",
+        href: "#childlink10",
+      },
+      {
+        id: "l11",
+        text: "Child Link 11",
+        href: "#childlink11",
+      },
+      {
+        id: "l12",
+        text: "Child Link 12",
+        href: "#childlink12",
+      },
+      {
+        id: "l13",
+        text: "Child Link 13",
+        href: "#childlink13",
+      },
+      {
+        id: "l14",
+        text: "Child Link 14",
+        href: "#childlink14",
+      },
+      {
+        id: "l6",
+        text: "Child Link 6",
+        href: "#childlink6",
+      },
+      {
+        id: "l7",
+        text: "Child Link 7",
+        href: "#childlink7",
+      },
+      {
+        id: "l8",
+        text: "Child Link 8",
+        href: "#childlink8",
+      },
+      {
+        id: "l9",
+        text: "Child Link 9",
+        href: "#childlink9",
+      },
+      {
+        id: "l10",
+        text: "Child Link 10",
+        href: "#childlink10",
+      },
+      {
+        id: "l11",
+        text: "Child Link 11",
+        href: "#childlink11",
+      },
+      {
+        id: "l12",
+        text: "Child Link 12",
+        href: "#childlink12",
+      },
+      {
+        id: "l13",
+        text: "Child Link 13",
+        href: "#childlink13",
+      },
+      {
+        id: "l14",
+        text: "Child Link 14",
+        href: "#childlink14",
+      },
+      {
+        id: "l6",
+        text: "Child Link 6",
+        href: "#childlink6",
+      },
+      {
+        id: "l7",
+        text: "Child Link 7",
+        href: "#childlink7",
+      },
+      {
+        id: "l8",
+        text: "Child Link 8",
+        href: "#childlink8",
+      },
+      {
+        id: "l9",
+        text: "Child Link 9",
+        href: "#childlink9",
+      },
+      {
+        id: "l10",
+        text: "Child Link 10",
+        href: "#childlink10",
+      },
+      {
+        id: "l11",
+        text: "Child Link 11",
+        href: "#childlink11",
+      },
+      {
+        id: "l12",
+        text: "Child Link 12",
+        href: "#childlink12",
+      },
+      {
+        id: "l13",
+        text: "Child Link 13",
+        href: "#childlink13",
+      },
+      {
+        id: "l14",
+        text: "Child Link 14",
+        href: "#childlink14",
+      },
+      {
+        id: "l6",
+        text: "Child Link 6",
+        href: "#childlink6",
+      },
+      {
+        id: "l7",
+        text: "Child Link 7",
+        href: "#childlink7",
+      },
+      {
+        id: "l8",
+        text: "Child Link 8",
+        href: "#childlink8",
+      },
+      {
+        id: "l9",
+        text: "Child Link 9",
+        href: "#childlink9",
+      },
+      {
+        id: "l10",
+        text: "Child Link 10",
+        href: "#childlink10",
+      },
+      {
+        id: "l11",
+        text: "Child Link 11",
+        href: "#childlink11",
+      },
+      {
+        id: "l12",
+        text: "Child Link 12",
+        href: "#childlink12",
+      },
+      {
+        id: "l13",
+        text: "Child Link 13",
+        href: "#childlink13",
+      },
+      {
+        id: "l14",
+        text: "Child Link 14",
+        href: "#childlink14",
+      },
+    ],
+  },
+  {
+    id: "l15",
+    text: "Link 15",
+    href: "#link15",
+    children: [
+      {
+        id: "l16",
+        text: "Child Link 16",
+        href: "#childlink16",
+      },
+      {
+        id: "l17",
+        text: "Child Link 17",
+        href: "#childlink17",
         children: [
           {
-            id: "l9",
-            text: "Child Link 9",
-            href: "#childlink9",
+            id: "l18",
+            text: "Child Link 18",
+            href: "#childlink18",
           },
           {
-            id: "l10",
-            text: "Child Link 11",
-            href: "#childlink12",
+            id: "l19",
+            text: "Child Link 19",
+            href: "#childlink19",
+          },
+          {
+            id: "l20",
+            text: "Child Link 20",
+            href: "#childlink20",
+          },
+          {
+            id: "l21",
+            text: "Child Link 21",
+            href: "#childlink21",
+          },
+          {
+            id: "l22",
+            text: "Child Link 22",
+            href: "#childlink22",
+          },
+          {
+            id: "l23",
+            text: "Child Link 23",
+            href: "#childlink23",
           },
         ],
       },
