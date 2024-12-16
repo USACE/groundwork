58ef2e3ccb647c9717c9c4f7c6f57e0cbee12fa5
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-12-16 10:04:50 -0500

    Forward ref in Link component
    Need to forward ref for Link to maintain compatibility with the
    redux-bundler routing pattern.

--- lib/components/navigation/link.jsx
+++ lib/components/navigation/link.jsx
@@ -1,16 +1,20 @@
 import { useLinkComponent } from "./link-provider";
+import { forwardRef } from "react";
 
-const Link = ({ href, children, ...props }) => {
+const Link = ({ href, children, ...props }, ref) => {
   const { component: Component, hrefMap } = useLinkComponent();
 
   const ProvidedLink = Component || "a";
   const hrefProp = hrefMap || "href";
 
   return (
-    <ProvidedLink {...{ [hrefProp]: href }} {...props}>
+    <ProvidedLink {...{ [hrefProp]: href }} {...props} ref={ref}>
       {children}
     </ProvidedLink>
   );
 };
 
-export default Link;
+const ForwardedLink = forwardRef(Link);
+export { ForwardedLink as Link };
+
+export default forwardRef(Link);
