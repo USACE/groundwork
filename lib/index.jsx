04108bdf3049fa3500ab6967022b6b99cc1f5185
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2024-12-16 12:06:17 -0600

    Create progress component and add import for lib

--- lib/index.jsx
+++ lib/index.jsx
@@ -15,6 +15,7 @@ export * from "./components/table";
 export * from "./components/display/card";
 export * from "./components/profile-dropdown";
 export { Skeleton } from "./components/display/skeleton";
+export { ProgressBar } from "./components/display/progress-bar";
 
 // composite components
 export { Header } from "./composite/header";
