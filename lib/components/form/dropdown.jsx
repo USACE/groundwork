b5c03dc66ef95eb7c041ba880fc551f9d9751a75
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2025-09-15 23:09:45 -0500

    Remove internal state for passed in value/defaultValue/props

--- lib/components/form/dropdown.jsx
+++ lib/components/form/dropdown.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import gwMerge from "../../gw-merge";
 
 function Dropdown({
@@ -7,9 +6,10 @@ function Dropdown({
   labelClassName = "",
   className = "",
   onChange,
+  value,
+  defaultValue,
   ...props
 }) {
-  const [selectedValue, setSelectedValue] = useState(options[0]?.value);
   if (!options || options.length === 0) {
     console.error(
       `Dropdown ${label} must have at least one option with value and text.`,
@@ -31,17 +31,14 @@ function Dropdown({
       <select
         id={label}
         name={label}
-        onChange={(e) => {
-          // Control the state if a value is not provided
-          !props?.value && setSelectedValue(e.target.value);
-          onChange(e);
-        }}
+        onChange={onChange}
         className={gwMerge(
           "gw-mt-2 gw-block gw-w-full gw-rounded-md gw-border-0 gw-py-1.5 gw-pl-3 gw-pr-10 gw-text-gray-900 gw-ring-1 gw-ring-inset gw-ring-gray-300 focus:gw-ring-2 focus:gw-ring-indigo-600 sm:gw-text-sm sm:gw-leading-6",
           className,
         )}
+        value={value}
+        defaultValue={defaultValue}
         {...props}
-        value={props?.value || selectedValue}
       >
         {options}
       </select>
