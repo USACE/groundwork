b3c3b8639a7571ec5e923de315282780c74fca4d
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2025-10-07 11:10:47 -0500

    update profile dropdown to fix gw: issues and update import from headlessui

--- lib/components/profile-dropdown.jsx
+++ lib/components/profile-dropdown.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Menu } from "@headlessui/react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import Link from "./navigation/link";
 
 function sha256(plain) {
@@ -42,10 +42,11 @@ function ProfileDropdown({
   showLogout = false,
   onLogout,
 }) {
+  const showMenu = links.length > 0 || showLogout;
   return (
     <Menu as="div" className="gw:relative gw:ml-4 gw:shrink-0">
       <div>
-        <Menu.Button className="gw:relative gw:flex gw:rounded-full gw:bg-white gw:focus:gw:outline-none">
+        <MenuButton className="gw:relative gw:flex gw:rounded-full gw:bg-white gw:focus:gw:outline-none">
           <span className="gw:absolute gw:-inset-1.gw:5" />
           <span className="gw:sr-only">Open user menu</span>
           {email ? (
@@ -57,32 +58,32 @@ function ProfileDropdown({
               </span>
             </strong>
           )}
-        </Menu.Button>
+        </MenuButton>
       </div>
-      {links && links.length > 0 ? (
-        <Menu.Items className="gw:absolute gw:right-0 gw:z-10 gw:mt-2 gw:w-48 gw:origin-top-right gw:rounded-sm gw:bg-white gw:py-1 gw:shadow-lg gw:ring-1 gw:ring-black gw:ring-opacity-5 gw:focus:gw:outline-none">
+      {showMenu ? (
+        <MenuItems className="gw:absolute gw:right-0 gw:z-10 gw:mt-2 gw:w-48 gw:origin-top-right gw:rounded-sm gw:bg-white gw:py-1 gw:shadow-lg gw:ring-1 gw:ring-black/5 gw:focus:gw:outline-none">
           {links.map((item) => (
-            <Menu.Item key={item.text}>
+            <MenuItem key={item.text}>
               <Link
                 key={item.id}
                 href={item.href}
-                className="gw:block gw:px-4 gw:py-2 gw:text-sm gw:text-gray-700 gw:hover:gw:bg-gray-100"
+                className="gw:block gw:px-4 gw:py-2 gw:text-sm gw:text-gray-700 gw:hover:bg-gray-100"
               >
                 {item.text}
               </Link>
-            </Menu.Item>
+            </MenuItem>
           ))}
           {showLogout ? (
-            <Menu.Item>
+            <MenuItem>
               <button
                 onClick={onLogout}
-                className="gw:block gw:w-full gw:px-4 gw:py-2 gw:text-sm gw:text-left gw:text-gray-700 gw:hover:gw:bg-gray-100"
+                className="gw:block gw:w-full gw:px-4 gw:py-2 gw:text-sm gw:text-left gw:text-gray-700 gw:hover:bg-gray-100"
               >
                 Logout
               </button>
-            </Menu.Item>
+            </MenuItem>
           ) : null}
-        </Menu.Items>
+        </MenuItems>
       ) : null}
     </Menu>
   );
