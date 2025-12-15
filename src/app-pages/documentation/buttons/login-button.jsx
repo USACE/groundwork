3dc6bf19615ae604537232438d6aa3dad4709035
Charles Graham SWT <krowven@gmail.com>
2025-12-15 10:26:11 -0600

    Add missing baseurl to logout/login-button

--- src/app-pages/documentation/buttons/login-button.jsx
+++ src/app-pages/documentation/buttons/login-button.jsx
@@ -11,18 +11,20 @@ import PropsTable from "../../../app-components/props-table";
 import DocsPage from "../_docs-page";
 import { useState } from "react";
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 const pageBreadcrumbs = [
   {
     text: "Documentation",
-    href: "#/docs",
+    href: `${BASE_URL}#/docs`,
   },
   {
     text: "Buttons",
-    href: "#/docs/buttons",
+    href: `${BASE_URL}#/docs/buttons`,
   },
   {
     text: "Login Button",
-    href: "#/docs/buttons/login-button",
+    href: `${BASE_URL}#/docs/buttons/login-button`,
   },
 ];
 
